8a790a5c6371c08693b00f609918de504f750907
dearestdust071@gmail.com <dearestdust071@gmail.com>
2023-10-18 09:31:55 -0600

    Config inicial -J

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+// import { PrivateComponent } from './private/private.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    // PrivateComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
