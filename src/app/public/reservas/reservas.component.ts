863ad0f2f598f11bd281c68cd089936c23589ec7
DanyH2002 <danielacrisantoluna@gmail.com>
2023-11-21 01:12:01 -0600

    reservacion -- H

--- src/app/public/reservas/reservas.component.ts
+++ src/app/public/reservas/reservas.component.ts
@@ -17,24 +17,18 @@ import { Room } from '../habitaciones/Room.model';
   ],
 })
 export class ReservasComponent {
-  habitacionSeleccionada: any = null;
+  habitacionSeleccionada: Room | null = null;
+
+  constructor(private habitacionesService: HabitacionesService,private router: Router) {}
 
   calcularSubtotal(fechaInicio: string, fechaFin: string): void {
-    // Obtener el costo por noche de la habitación seleccionada
     if (this.habitacionSeleccionada) {
       const costoPorNoche = this.habitacionSeleccionada.costo;
-
       const fechaInicioMs = new Date(fechaInicio).getTime();
       const fechaFinMs = new Date(fechaFin).getTime();
       const tiempoReservaMs = fechaFinMs - fechaInicioMs;
-
-      // Calcular la cantidad de días de la reserva
       const diasReserva = Math.ceil(tiempoReservaMs / (1000 * 60 * 60 * 24));
-
-      // Calcular el subtotal multiplicando los días por el costo por noche
       const subtotal = diasReserva * costoPorNoche;
-
-      // Asignar el subtotal al campo correspondiente en el formulario
       const subtotalInput = document.getElementById('subtotal') as HTMLInputElement;
       subtotalInput.value = subtotal.toString();
     } else {
@@ -42,5 +36,26 @@ export class ReservasComponent {
     }
   }
 
+  seleccionarHabitacion(habitacion: Room): void {
+    this.habitacionSeleccionada = habitacion;
+  }
 
+  obtenerHabitaciones(): void {
+    this.habitacionesService.getHabitaciones().subscribe(
+      (response: any) => {
+        const habitaciones: Room[] = response.habitaciones;
+        console.log(habitaciones);
+      },
+      (error: any) => {
+        console.error('Error al obtener las habitaciones:', error);
+      }
+    );
+  }
+  ngOnInit(): void {
+    this.obtenerHabitaciones();
+  }
+
+  irAPago(){
+    this.router.navigate(['']);
+  }
 }
